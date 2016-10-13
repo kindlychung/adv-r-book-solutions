6ff0410656f0645c7269eaf1009ae5ee955e1aa3
Kaiyin Zhong <kindlychung@gmail.com>
2016-10-13 21:00:04 +0200

    Update exercise2.r

--- 13_expressions/02_names/exercise2.r
+++ 13_expressions/02_names/exercise2.r
@@ -1,25 +1,16 @@
 ### Write an equivalent to get() using as.name() and eval().
 ### (Don’t worry about the multiple ways of choosing an environment; assume that
 ### the user supplies it explicitly.)
-
-get2 <- function(x, env) {
-  eval(as.name(x), envir = env)
+x = 3.13 
+get1 <- function(x) {
+  # eval(as.name(x), enclos = parent.frame())
+  force(x)
+  eval(as.name(x), parent.frame())
 }
-
-x <- 10
-get("x", env = .GlobalEnv)
-# [1] 10
-get2("x", env = .GlobalEnv)
-# [1] 10
-
-### Write an equivalent to assign() using as.name(), substitute(), and eval().
-assign2 <- function(x, value, env) {
-  eval(substitute(x <- value,list(x = as.name(x), value = value)), env)
-  if (length(x) > 1)
-    warning('only the first element is used as variable name')
+get1("x")
+assign1 <- function(x, x_value) {
+  x_name = as.name(x)
+  eval(substitute(x <- v, list(x = x_name, v = x_value)), parent.frame())
 }
-## somewhat convoluted?
-e <- new.env()
-assign2('x', 3, e)
-e$x
-# [1] 3
+assign1("y", 111)
+y
